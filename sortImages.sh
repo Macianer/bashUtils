c11fb9a4e5f7890bef980bdf1546941d63e5316e
ronnymeissner <owner@meissner-ronny.de>
2014-09-20 19:43:16 +0200

    update scripts

---
+++ sortImages.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# main.sh
+# Author: Ronny Meißner
+#
+
+source subs.sh
+
+exivSortTargetDirIntoSourceByYearMonth  "../testInput" "../Photos" "jpg"
+exivSortTargetDirIntoSourceByYearMonth  "../testInput" "../Photos" "png"
